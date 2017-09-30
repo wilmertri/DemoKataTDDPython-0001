584f02fa381762f7d6c01fae510b6141e39241be
Wilmer Triana <fabiantriana1072@gmail.com>
2017-09-30 18:55:58 -0500

    Paso 2 - Se agrega prueba para probar con un número (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,3 +5,6 @@ from Calculadora import Calculadora
 class CalculadoraTest(TestCase):
     def test_sumar(self):
         self.assertEqual(Calculadora().sumar(""), 0, "Cadena vacia")
+
+    def test_sumar_unacadena(self):
+        self.assertEqual(Calculadora().sumar("1"), 1, "Un número")
