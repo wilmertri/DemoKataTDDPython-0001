659ef608ffd7c9b3a12783a9fc98d3da4432ba0e
Wilmer Triana <fabiantriana1072@gmail.com>
2017-09-30 20:24:54 -0500

    Paso 4 - Se agrega un test para probar el caso String con multiples numeros (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -15,3 +15,6 @@ class CalculadoraTest(TestCase):
 
     def test_sumar_cadenaConDosNumeros(self):
         self.assertEqual(Calculadora().sumar("1,3"), 4, "Dos numeros")
+
+    def test_sumar_cadenaConMultiplesNumeros(self):
+        self.assertEqual(Calculadora().sumar("5,2,4,1"), 12, "Multiples numeros")
