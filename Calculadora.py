a9b4bc369fef0711f80064ba2f8eab1fb5616b7c
Wilmer Triana <fabiantriana1072@gmail.com>
2017-09-30 20:46:38 -0500

    Paso 5 - Se modigica código para que la prueba suma de multiples numeros con distintos separadores pase (Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -4,7 +4,9 @@ class Calculadora:
     def sumar(self, cadena):
         if cadena == "":
             return 0
-        elif "," in cadena:
+        elif "," in cadena or "&" in cadena or ":" in cadena:
+            cadena = cadena.replace("&", ",")
+            cadena = cadena.replace(":", ",")
             numeros = cadena.split(",")
             suma = 0
             for num in numeros:
